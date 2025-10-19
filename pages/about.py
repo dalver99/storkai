2da32cb3850018e9986133e35aa1be63b98e3264
davler99 <jykevinkim@gmail.com>
2025-10-19 23:40:44 +0900

    time to start about the core structure.."

--- pages/about.py
+++ pages/about.py
@@ -5,3 +5,13 @@ def about():
     st.set_page_config(page_title="📈 About!!", layout="wide")
     st.title("About")
     st.write("This is the about page")
+
+    st.write
+    st.write("Developed by Kevin / Junyoung Kim")
+    st.write("I am a Korean stock investor, and I have been investing in Korean stocks for a long time.")
+    st.write("This is my attempt to provide assistance investing in Korean stocks, in a way I invest.")
+
+    st.write("---")
+
+
+about()
